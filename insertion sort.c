d86952d3b2d1299af82e19098c3c5ae3fc8d15c6
TChaitanya999 <126843312+TChaitanya999@users.noreply.github.com>
2023-03-10 09:26:55 +0530

    insertion.c

--- insertion sort.c
+++ insertion sort.c
@@ -8,22 +8,25 @@ int main()
    for(i=0;i<n;i++)
       {
         printf("\n Enter the %d element=",i+1);
-            scanf("%d",&arr[i]);
+        scanf("%d",&arr[i]);
       }
+      // code for insertion sort
     for(i=1;i<n;i++)
       {
-               key=arr[i];
-           j=i-1;
-     }
-          while(key<arr[j]&&j>=0)
+        key=arr[i];
+        j=i-1;
+        while(key < arr[j] && j>=0)
                {
-          arr[j+1]=arr[j];
-            j=j-1;
-             }
-       arr[j+1]=key;
-    printf(" array of %d iteration",i+1);
+                arr[j+1]=arr[j];
+                j=j-1;
+                }
+            arr[j+1]=key;
+        }
+    
+    //end of the code for insertion sort
+    printf(" Sorted array is ");
       for(j=0;j<n;j++)
-    printf("\t%d",arr[i]);
+        printf("\t%d",arr[j]);
           return 0;
 
  }
