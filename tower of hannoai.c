4e005697d4e2016499b4fbd15f45ecfeb3c6576d
TChaitanya999 <126843312+TChaitanya999@users.noreply.github.com>
2023-03-17 08:33:00 +0530

    Add files via upload
    uploded

---
+++ tower of hannoai.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+  
+void TOH(int n,char x,char y, char z)
+ 
+{
+ 
+    if(n>0)
+
+      {
+     
+           TOH(n-1,x,y,z);
+   
+           printf("\n%c to %c",x,y);
+   
+        TOH(n-1,z,y,x);
+
+       }
+    
+}
+    
+ int main()
+  
+        {
+    
+            int n=3;
+    
+           TOH(n,'A','B','C');
+
+   }
+out put:
+A to B
+A to B
+C to B
+A to B
+C to B
+C to B
+A to B
